31a26f0da797b3651bc7ddae897d0759b164879a
zhiguangq <qiuzhiguang@kugou.net>
2015-10-13 12:04:14 +0800

    mv _WIN define to cpp file

--- src/srs/srs_librtmp.h
+++ src/srs/srs_librtmp.h
@@ -1036,61 +1036,6 @@ typedef void* srs_hijack_io_t;
     extern int srs_hijack_io_write(srs_hijack_io_t ctx, void* buf, size_t size, ssize_t* nwrite);
 #endif
 
-/*************************************************************
-**************************************************************
-* Windows SRS-LIBRTMP solution
-**************************************************************
-*************************************************************/
-// for srs-librtmp, @see https://github.com/simple-rtmp-server/srs/issues/213
-#ifdef _WIN32
-    // for time.
-    #define _CRT_SECURE_NO_WARNINGS
-    #include <time.h>
-    int gettimeofday(struct timeval* tv, struct timezone* tz);
-    #define PRId64 "lld"
-    
-    // for inet helpers.
-    typedef int socklen_t;
-    const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
-    
-    // for mkdir().
-    #include<direct.h>
-    
-    // for open().
-    typedef int mode_t;
-    #define S_IRUSR 0
-    #define S_IWUSR 0
-    #define S_IXUSR 0
-    #define S_IRGRP 0
-    #define S_IWGRP 0
-    #define S_IXGRP 0
-    #define S_IROTH 0
-    #define S_IXOTH 0
-    
-    // for file seek.
-    #include <io.h>
-    #include <fcntl.h>
-    #define open _open
-    #define close _close
-    #define lseek _lseek
-    #define write _write
-    #define read _read
-    
-    // for pid.
-    typedef int pid_t;
-    pid_t getpid(void);
-    
-    // for socket.
-    ssize_t writev(int fd, const struct iovec *iov, int iovcnt);
-    typedef int64_t useconds_t;
-    int usleep(useconds_t usec);
-    int socket_setup();
-    int socket_cleanup();
-    
-    // others.
-    #define snprintf _snprintf
-#endif
-
 #ifdef __cplusplus
 }
 #endif
